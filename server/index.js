e0de2b6e82150f8e9ead0b1b2d724fafb2d841a7
I-Venkatesh <venkatesh.i18@iiits.in>
2021-05-11 15:43:30 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
+const cors = require('cors');
+
+const PORT = process.env.PORT || 5000;
+
+const router = require('./router');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use(cors());
+
+io.on('connection', (socket) => {
+    console.log('We have a new connection!!!');
+    socket.on('disconnect', () => {
+        console.log('User had left!!!');
+    })
+})
+ 
+
+app.use(router);
+
+server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
